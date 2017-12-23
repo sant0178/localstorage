54d66ef5e8c0c9653de13afb5a27de8b6c3ce77b
JS50 <sant0178@algonquinlive.com>
2017-12-23 00:10:40 -0500

    here

---
+++ starter.js
@@ -0,0 +1,5 @@
+let contactStarter =[
+    {"fullname":"Han Solo", "phone":"613-235-1242", "email":"han.solo@google.com"},
+    {"fullname":"Luke Skywalker","phone":"613-465-5456", "email":"luke.skywalker@google.com"},
+    {"fullname":"Kylo Ren", "phone":"613-907-8612", "email":"kylo.ren@google.com"}
+];
